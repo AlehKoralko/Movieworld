343fe64890b526060c26396c6939212e83dd3620
Aleh Koralko <oleg.korolko.work@gmail.com>
2018-05-10 17:37:46 +0300

    Rewrote the addSession method

--- src/main/java/by/psu/service/impl/SessionServiceImpl.java
+++ src/main/java/by/psu/service/impl/SessionServiceImpl.java
@@ -1,7 +1,5 @@
 package by.psu.service.impl;
 
-import by.psu.dao.CinemaHallDao;
-import by.psu.dao.FilmDao;
 import by.psu.dao.SessionDao;
 import by.psu.model.Session;
 import by.psu.service.SessionService;
@@ -9,36 +7,21 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.time.LocalDate;
-import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
 import java.util.List;
 
 @Service
 public class SessionServiceImpl implements SessionService {
 
-    private DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
     private DateTimeFormatter timeFormatter = DateTimeFormatter.ofPattern("HH:mm");
     private DateTimeFormatter formatDateFormatter = DateTimeFormatter.ofPattern("dd MMMM yyyy");
 
     @Autowired
     private SessionDao sessionDao;
 
-    @Autowired
-    private FilmDao filmDao;
-
-    @Autowired
-    private CinemaHallDao cinemaHallDao;
-
     @Override
     @Transactional
     public void addSession(Session session) {
-        session.setDate(dateFormatter.parse(session.getFormatDate(), LocalDate::from));
-        session.setTime(timeFormatter.parse(session.getFormatTime(), LocalTime::from));
-
-        session.setFilm(filmDao.getById(Integer.valueOf(session.getFilmId())));
-        session.setCinemaHall(cinemaHallDao.getById(Integer.valueOf(session.getCinemaHallId())));
-
         sessionDao.add(session);
     }
 
