56715d19ec70ff98ecd0c0f79a1a956ea60a23e4
Aleh Koralko <oleg.korolko.work@gmail.com>
2018-05-10 17:33:35 +0300

    Renamed SessionValidator to SessionFormValidator

--- src/main/java/by/psu/validator/SessionFormValidator.java
+++ src/main/java/by/psu/validator/SessionFormValidator.java
@@ -1,6 +1,6 @@
 package by.psu.validator;
 
-import by.psu.model.Session;
+import by.psu.dto.SessionForm;
 import org.springframework.stereotype.Component;
 import org.springframework.validation.Errors;
 import org.springframework.validation.ValidationUtils;
@@ -11,39 +11,39 @@ import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
 
 @Component
-public class SessionValidator implements Validator {
+public class SessionFormValidator implements Validator {
 
     private DateTimeFormatter sessionDateformatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
     private DateTimeFormatter sessionTimeformatter = DateTimeFormatter.ofPattern("HH:mm");
 
     @Override
     public boolean supports(Class<?> aClass) {
-        return Session.class.equals(aClass);
+        return SessionForm.class.equals(aClass);
     }
 
     @Override
     public void validate(Object object, Errors errors) {
 
-        Session session = (Session) object;
+        SessionForm sessionForm = (SessionForm) object;
 
-        ValidationUtils.rejectIfEmptyOrWhitespace(errors, "formatDate", "Required");
-        ValidationUtils.rejectIfEmptyOrWhitespace(errors, "formatTime", "Required");
+        ValidationUtils.rejectIfEmptyOrWhitespace(errors, "date", "Required");
+        ValidationUtils.rejectIfEmptyOrWhitespace(errors, "time", "Required");
         ValidationUtils.rejectIfEmptyOrWhitespace(errors, "filmId", "Required");
         ValidationUtils.rejectIfEmptyOrWhitespace(errors, "cinemaHallId", "Required");
 
-        if (! errors.hasFieldErrors("formatDate")) {
+        if (! errors.hasFieldErrors("date")) {
             try {
-                sessionDateformatter.parse(session.getFormatDate(), LocalDate::from);
+                sessionDateformatter.parse(sessionForm.getDate(), LocalDate::from);
             } catch (Exception e) {
-                errors.rejectValue("formatDate", "Format.session.date");
+                errors.rejectValue("date", "Format.session.date");
             }
         }
 
-        if (! errors.hasFieldErrors("formatTime")) {
+        if (! errors.hasFieldErrors("time")) {
             try {
-                sessionTimeformatter.parse(session.getFormatTime(), LocalTime::from);
+                sessionTimeformatter.parse(sessionForm.getTime(), LocalTime::from);
             } catch (Exception e) {
-                errors.rejectValue("formatTime", "Format.session.time");
+                errors.rejectValue("time", "Format.session.time");
             }
         }
     }
